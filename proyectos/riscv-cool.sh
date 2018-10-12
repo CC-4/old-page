81b6f6e0eb66fe87ab726d7a1d32cbe3c7aab694
Andrés Castellanos <andres.cv@galileo.edu>
2018-10-12 11:58:03 -0600

    update riscv-cool.sh

--- proyectos/riscv-cool.sh
+++ proyectos/riscv-cool.sh
@@ -7,6 +7,7 @@ wget -O vsim https://git.io/fbpu0 && chmod +x vsim && . ./vsim && rm vsim
 
 # download traphandler
 cd ~/.vsim
+rm -f traphandler.s
 wget https://raw.githubusercontent.com/CC-4/traphandler/master/traphandler.s
 
 # download coolc-rv
@@ -15,7 +16,7 @@ wget https://github.com/CC-4/cc-4.github.io/raw/master/proyectos/coolc-rv.jar
 rm -f coolc-rv
 printf '#!/bin/sh\n\n' >> coolc-rv
 printf 'if [ "$#" -gt 0 ]; then\n' >> coolc-rv
-printf '  /usr/class/cs143/cool/etc/../lib/.i686/PA5J/lexer $* | /usr/class/cs143/cool/etc/../lib/.i686/PA5J/parser $* | /usr/class/cs143/cool/etc/../lib/.i686/PA5J/semant $* | java -classpath /usr/class/cs143/cool/lib:.:/usr/java/lib/rt.jar:/usr/class/cs143/cool/bin/coolc-rv.jar Cgen $*\n' >> coolc-rv
+printf '  /usr/class/cs143/cool/etc/../lib/.i686/PA5J/lexer $* | /usr/class/cs143/cool/etc/../lib/.i686/PA5J/parser $* | /usr/class/cs143/cool/etc/../lib/.i686/PA5J/semant $* | java -classpath /usr/class/cs143/cool/lib:/usr/java/lib/rt.jar:/usr/class/cs143/cool/bin/coolc-rv.jar Cgen $*\n' >> coolc-rv
 printf 'fi' >> coolc-rv
 chmod +x coolc-rv
 
