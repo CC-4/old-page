a0b84a8f3435dae48fe93c2ee48c0a692caa86d6
Andrés Castellanos <andres.cv@galileo.edu>
2019-09-03 18:20:30 -0600

    updates

--- projects/install_rv.sh
+++ projects/install_rv.sh
@@ -4,6 +4,7 @@ DIR="$(pwd)"
 
 cd /usr/class/cs143/bin
 
+rm -f coolc-rv.jar coolc-rv jupiter-cool
 wget https://raw.githubusercontent.com/CC-4/cc-4.github.io/master/projects/coolc-rv.jar
 wget https://raw.githubusercontent.com/CC-4/cc-4.github.io/master/projects/coolc-rv
 wget https://raw.githubusercontent.com/CC-4/cc-4.github.io/master/projects/jupiter-cool
@@ -11,6 +12,7 @@ chmod +x coolc-rv
 chmod +x jupiter-cool
 
 cd /usr/class/cs143/lib
+rm -f runtime.s
 wget https://raw.githubusercontent.com/CC-4/runtime/master/runtime.s
 
 
